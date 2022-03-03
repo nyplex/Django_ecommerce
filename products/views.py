13489e6fe35d78db8f86cbbbf653e5611bb6ec5b
Alex Nypels <nypels.alexandre@outlook.com>
2022-03-03 12:41:38 +0000

    Adding product details template and load data

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Product
 
 # Create your views here.
@@ -12,3 +12,15 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_details(request, product_id):
+    """ A view to show individual product details """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
