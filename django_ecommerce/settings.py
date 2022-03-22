bc58a9371ab8aa4f69395051cf10f43d0405e6f8
Alex Nypels <nypels.alexandre@outlook.com>
2022-03-22 14:07:17 +0000

    Adding basic checkout functionality using Stripe

--- django_ecommerce/settings.py
+++ django_ecommerce/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+if os.path.exists("env.py"):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -168,10 +170,17 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-FREE_DELIVERY_THRESHOLD = 50
-STANDARD_DELIVERY_PERCENTAGE = 10
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10
+STRIPE_CURRENCY = 'usd'
+
+STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY')
+STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ.setdefault("STRIPE_PUBLIC_KEY", "pk_test_51Kg6xRB5XP5EfZeUBXBGSl87DO6mX3UfCvZjtw7C9zosrTgdtf8QzE4AicQmAvQvkl9xpJq260AhcIUQfo0STf6500U8wyXaYJ")
+os.environ.setdefault("STRIPE_SECRET_KEY", "sk_test_51Kg6xRB5XP5EfZeUJwcNdLxIO2b66QIUW3yVNi96uSgTCmUiFZvqEWQJYsiU7fl0a2Mi9hfymqbmytgGrjA4iGzf00MRwua9Pb")
+
