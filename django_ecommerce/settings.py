1a37393c07a9fe1172162b9d28e41d63770c3c3f
Alex Nypels <nypels.alexandre@outlook.com>
2022-03-30 13:18:09 +0100

    fixing database url

--- django_ecommerce/settings.py
+++ django_ecommerce/settings.py
@@ -123,10 +123,10 @@ WSGI_APPLICATION = 'django_ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if 'POSTGRES_URL' in os.environ:
-    POSTGRES_URL = os.environ.get('POSTGRES_URL')
+if 'DATABASE_URL' in os.environ:
+    DATABASE_URL = os.environ.get('DATABASE_URL')
     DATABASES = {
-        'default': dj_database_url.parse(POSTGRES_URL)
+        'default': dj_database_url.parse(DATABASE_URL)
     }
     
 else:
