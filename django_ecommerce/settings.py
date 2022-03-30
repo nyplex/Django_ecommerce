c2011f7219b55b8d6eb0002065e2cb9721383483
Alex Nypels <nypels.alexandre@outlook.com>
2022-03-30 13:23:01 +0100

    Removed secret key and set debug

--- django_ecommerce/settings.py
+++ django_ecommerce/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ju79c@quze(ter^ic5_+mhse_vn*&w6kc3nedi=p(#(r7#%2pu'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['nyplex-django-ecommerce.herokuapp.com', 'localhost', '127.0.0.1']
 
